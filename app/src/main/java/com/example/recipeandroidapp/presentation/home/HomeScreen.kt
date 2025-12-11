a0a09a77ab15e36831aa06e25c75fccd46a690e3
AndriyUkrainets <andriy.ukrainetc.school@gmail.com>
2025-12-11 17:53:20 +0200

    feature: change Home screen recipes on daily recipes

--- app/src/main/java/com/example/recipeandroidapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/recipeandroidapp/presentation/home/HomeScreen.kt
@@ -1,46 +1,67 @@
 package com.example.recipeandroidapp.presentation.home
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.paging.compose.LazyPagingItems
 import com.example.recipeandroidapp.domain.model.Recipe
-import com.example.recipeandroidapp.presentation.navgraph.Route
+import com.example.recipeandroidapp.ui.common.EmptyScreen
+import com.example.recipeandroidapp.ui.common.RecipeCardShimmerEffect
 import com.example.recipeandroidapp.ui.common.RecipeCardsList
 import com.example.recipeandroidapp.ui.common.SearchBar
+import com.example.recipeandroidapp.util.Dimens.MediumPadding1
+import com.example.recipeandroidapp.util.Dimens.SmallPadding1
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
-    recipes: LazyPagingItems<Recipe>,
-    navigation: (String) -> Unit
+    state: DailyRecipesState,
+    navigateToSearch: () -> Unit,
+    navigateToDetails: (Recipe) -> Unit,
+    navigateToFilter: () -> Unit,
 ) {
     Column(
         modifier = Modifier
-            .fillMaxSize()
+            .padding(
+                top = MediumPadding1,
+                start = MediumPadding1,
+                end = MediumPadding1
+            )
             .statusBarsPadding()
+            .fillMaxSize()
     ) {
 
         SearchBar(
-            modifier = Modifier.fillMaxWidth(),
             searchText = "Tap to search",
             readOnly = true,
             onTextChange = { },
-            onFilterClick = { },
-            onSearchClick = {
-                navigation(Route.SearchScreen.route)
-            },
-            onSearch = {
-                // TODO: perform search
-            })
-
-        RecipeCardsList(
-            recipes = recipes, onClick = {
-                navigation(Route.DetailsScreen.route)
-            }
+            onFilterClick = navigateToFilter,
+            onSearchClick = navigateToSearch,
+            onSearch = {}
         )
+
+        Spacer(modifier = Modifier.height(MediumPadding1))
+
+        when {
+            state.isLoading -> {
+                Column(verticalArrangement = Arrangement.spacedBy(SmallPadding1)) {
+                    repeat(3){
+                        RecipeCardShimmerEffect()
+                    }
+                }
+            }
+
+            state.error != null -> {
+                EmptyScreen(
+                    error = state.error
+                )
+            }
+
+            else -> {
+                RecipeCardsList(
+                    recipes = state.recipes,
+                    onClick = { navigateToDetails(it) }
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/example/recipeandroidapp/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/recipeandroidapp/presentation/home/HomeViewModel.kt
@@ -2,67 +2,45 @@ package com.example.recipeandroidapp.presentation.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
-import androidx.paging.cachedIn
-import com.example.recipeandroidapp.domain.model.FilterParams
-import com.example.recipeandroidapp.domain.model.Recipe
 import com.example.recipeandroidapp.domain.usecases.RecipeUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.collections.emptyList
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val recipeUseCases: RecipeUseCases
-): ViewModel() {
+) : ViewModel() {
 
-    private val _tagIds = MutableStateFlow<Set<Int>>(emptySet())
-    private val _categoryIds = MutableStateFlow<Set<Int>>(emptySet())
-    private val _minTime = MutableStateFlow<Int?>(null)
-    private val _maxTime = MutableStateFlow<Int?>(null)
-    private val _searchQuery = MutableStateFlow<String?>(null)
+    private val _state = MutableStateFlow(DailyRecipesState())
+    val state: StateFlow<DailyRecipesState> = _state
 
-    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
-    val recipes: StateFlow<PagingData<Recipe>> = combine(
-        _tagIds,
-        _categoryIds,
-        _minTime,
-        _maxTime,
-        _searchQuery
-    ) { tags, categories, min, max, query ->
-        FilterParams(tags, categories, min, max, query)
-    }
-        .flatMapLatest { params ->
-            recipeUseCases.getRecipes(
-                tagIds = params.tagIds,
-                categoryIds = params.categoryIds,
-                minTime = params.minTime,
-                maxTime = params.maxTime,
-                search = params.search
+    fun loadDailyRecipes() {
+        viewModelScope.launch {
+            _state.value = _state.value.copy(
+                isLoading = true,
+                error = null
             )
+
+            try {
+                recipeUseCases.getDailyRecipes().collect { list ->
+                    _state.value = _state.value.copy(
+                        isLoading = false,
+                        recipes = list,
+                        error = null
+                    )
+                }
+
+            } catch (e: Exception) {
+                _state.value = _state.value.copy(
+                    isLoading = false,
+                    recipes = emptyList(),
+                    error = e.message ?: "Unexpected error"
+                )
+            }
         }
-        .cachedIn(viewModelScope)
-        .stateIn(viewModelScope, SharingStarted.Lazily, PagingData.empty())
-
-    fun setTags(tags: Set<Int>) {
-        _tagIds.value = tags
-    }
-
-    fun setCategories(categories: Set<Int>) {
-        _categoryIds.value = categories
-    }
-
-    fun setTimeRange(min: Int?, max: Int?) {
-        _minTime.value = min
-        _maxTime.value = max
-    }
-
-    fun setSearch(query: String?) {
-        _searchQuery.value = query
     }
 }
