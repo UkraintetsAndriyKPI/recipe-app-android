7fc7be3c68531f309c0f2ad450ee2092d5e3c4f8
AndriyUkrainets <andriy.ukrainetc.school@gmail.com>
2025-12-12 00:46:51 +0200

    feature: add route for filters and fixed search

--- app/src/main/java/com/example/recipeandroidapp/presentation/navigator/Navigator.kt
+++ app/src/main/java/com/example/recipeandroidapp/presentation/navigator/Navigator.kt
@@ -1,5 +1,6 @@
 package com.example.recipeandroidapp.presentation.navigator
 
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -7,7 +8,6 @@ import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -29,6 +29,8 @@ import com.example.recipeandroidapp.presentation.bookmark.BookmarkScreen
 import com.example.recipeandroidapp.presentation.bookmark.BookmarkViewModel
 import com.example.recipeandroidapp.presentation.detail.DetailScreen
 import com.example.recipeandroidapp.presentation.detail.DetailViewModel
+import com.example.recipeandroidapp.presentation.filters.FilterScreen
+import com.example.recipeandroidapp.presentation.filters.FilterViewModel
 import com.example.recipeandroidapp.presentation.home.HomeScreen
 import com.example.recipeandroidapp.presentation.home.HomeViewModel
 import com.example.recipeandroidapp.presentation.navgraph.Route
@@ -93,18 +95,56 @@ fun Navigator(modifier: Modifier = Modifier) {
                     state = state,
                     navigateToSearch = { navigateToTab(navController, Route.SearchScreen.route) },
                     navigateToDetails = { recipe -> navigateToDetails(navController, recipe) },
-                    navigateToFilter = { navController.navigate(Route.FilterScreen.route) }
+                    navigateToFilter = { navigateToTab(navController, Route.SearchScreen.route) }
                 )
             }
 
             composable(route = Route.SearchScreen.route) {
                 val viewModel: SearchViewModel = hiltViewModel()
-                val recipes = viewModel.recipes.collectAsLazyPagingItems()
+
+                val tags = navController.currentBackStackEntry
+                    ?.savedStateHandle
+                    ?.get<Set<Int>>("selectedTags")
+
+                val categories = navController.currentBackStackEntry
+                    ?.savedStateHandle
+                    ?.get<Set<Int>>("selectedCategories")
+
+                Log.d("NAVIGATOR SearchScreen", "SavedTags received: $tags")
+                Log.d("NAVIGATOR SearchScreen", "SavedCategories received: $categories")
+
+                LaunchedEffect(tags, categories) {
+                    if ((tags != null) && (categories != null)) {
+
+                        Log.d("NAVIGATOR SearchScreen", "Applying filters -> tags: $tags, categories: $categories")
+
+                        viewModel.setTags(tags)
+                        viewModel.setCategories(categories)
+
+                        navController.currentBackStackEntry
+                            ?.savedStateHandle
+                            ?.remove<Set<Int>>("selectedTags")
+                        navController.currentBackStackEntry
+                            ?.savedStateHandle
+                            ?.remove<Set<Int>>("selectedCategories")
+
+                        Log.d("NAVIGATOR SearchScreen", "selectedTags and selectedCategories removed from SavedStateHandle")
+                    } else {
+                        Log.d("NAVIGATOR SearchScreen", "savedFilters == null")
+                    }
+                }
+
                 SearchScreen(
-                    state = viewModel.state.value,
-                    recipes = recipes,
+                    viewModel = viewModel,
                     event = viewModel::onEvent,
                     navigateToDetails = { recipe -> navigateToDetails(navController, recipe) },
+                    navigateToFilter = {
+                        navigateToFilter(
+                            navController = navController,
+                            tags = viewModel.state.value.selectedTags,
+                            categories = viewModel.state.value.selectedCategories,
+                        )
+                    },
                 )
             }
 
@@ -135,14 +175,20 @@ fun Navigator(modifier: Modifier = Modifier) {
             }
 
             composable(route = Route.FilterScreen.route) {
-//                val viewModel: FilterViewModel = hiltViewModel()
-//
-//                FilterScreen(
-//                    state = viewModel.state.value,
-//                    event = viewModel::onEvent,
-//                    navigateUp = { navController.navigateUp() }
-//                )
-                Text("TODO FILTER SCREEN")
+                val viewModel: FilterViewModel = hiltViewModel()
+
+                FilterScreen(
+                    viewModel = viewModel,
+                    event = viewModel::onEvent,
+                    navigateUp = { navController.navigateUp() },
+                    onApply = { selectedCategories, selectedTags ->
+                        applyFiltersAndNavigateUp(
+                            navController = navController,
+                            selectedCategories = selectedCategories,
+                            selectedTags = selectedTags
+                        )
+                    }
+                )
             }
         }
 
@@ -166,4 +212,40 @@ private fun navigateToTab(navController: NavController, route: String) {
 private fun navigateToDetails(navController: NavController, recipe: Recipe) {
     navController.currentBackStackEntry?.savedStateHandle?.set("recipe", recipe)
     navController.navigate(route = Route.DetailsScreen.route)
+}
+
+private fun navigateToFilter(
+    navController: NavController,
+    tags: Set<Int>,
+    categories: Set<Int>
+) {
+    navController.currentBackStackEntry?.savedStateHandle?.set("selectedTags", tags)
+    navController.currentBackStackEntry?.savedStateHandle?.set("selectedCategories", categories)
+
+    navController.navigate(Route.FilterScreen.route) {
+        navController.graph.startDestinationRoute?.let { homeScreen ->
+            popUpTo(homeScreen) {
+                inclusive = true
+                saveState = true
+            }
+            restoreState = true
+            launchSingleTop = true
+        }
+    }
+}
+
+private fun applyFiltersAndNavigateUp(
+    navController: NavController,
+    selectedCategories: Set<Int>,
+    selectedTags: Set<Int>
+) {
+    Log.d("Filters", "Applying filters:")
+    Log.d("Filters", "Selected Tags: $selectedTags")
+    Log.d("Filters", "Selected Categories: $selectedCategories")
+
+    navController.previousBackStackEntry?.savedStateHandle?.set("selectedTags", selectedTags)
+    navController.previousBackStackEntry?.savedStateHandle?.set("selectedCategories", selectedCategories)
+
+    Log.d("Filters", "Filters saved. Navigating up...")
+    navController.navigateUp()
 }
